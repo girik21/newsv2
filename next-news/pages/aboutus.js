ff48690ff4bbf819164977c85580404eb0a707d4
Girik21 <girikaustuv@gmail.com>
2021-09-07 10:20:23 +0545

    Fix

---
+++ next-news/pages/aboutus.js
@@ -0,0 +1,39 @@
+import styles from '../styles/aboutus.module.css'
+import { Toolbar } from '../components/toolbar';
+
+export const aboutus = ({ employee }) => {
+console.log(employee);    
+  return (
+         <div className='pagecontainer'>
+             <Toolbar/> 
+         <div className={styles.main}>
+             <h1>About us</h1>
+
+             <div className={styles.main}>
+             <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image} alt="employee" />
+            <p>{employee.description}</p>
+             </div>
+         </div>
+         </div>
+         
+)
+  };
+
+  
+  
+  export const getServerSideProps = async pageContext => {
+    const apiResponse = await fetch(
+      'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+    );
+    const employee = await apiResponse.json();
+  
+    return {
+      props: {
+        employee,
+      },
+    };
+  };
+  
+  export default aboutus;
---
+++ next-news/pages/index.js
@@ -0,0 +1,17 @@
+import { Toolbar } from '../components/toolbar';
+import styles from '../styles/Home.module.css';
+
+
+export default function Home() {
+  return (
+    <div className="page-container">
+     <Toolbar/>
+
+      <div className={styles.main}>
+        <h1>News App</h1>
+
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
+  )
+}
