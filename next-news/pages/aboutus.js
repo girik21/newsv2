f4fe7cd543fa0f60b065191b6b76ade4757cd6d8
Girik21 <girikaustuv@gmail.com>
2021-09-12 15:03:22 +0545

    init

--- next-news/pages/aboutus.js
+++ next-news/pages/aboutus.js
@@ -7,7 +7,16 @@ export const aboutus = ({ employee }) => {
 console.log(employee);    
   return (
          <div className='pagecontainer'>
-            
+            <div>
+      <div className="px-2 md:px-24 bg-black text-white flex justify-between h-16">
+    <Link href="/">
+      <a className="text-white uppercase text-2xl flex align-baseline px-10 my-auto">
+        <GiAsianLantern className="icon" /> News24
+      </a>
+    </Link>
+    
+    </div>
+    </div>
            
              <Toolbar/> 
          <div className={styles.main}>
@@ -30,7 +39,7 @@ console.log(employee);
   
   export const getServerSideProps = async pageContext => {
     const apiResponse = await fetch(
-      'https://my-json-server.typicode.com/girik21/newsv2/employeeOfTheMonth',
+      'https://my-json-server.typicode.com/girik21/newsapp/aboutus',
     );
     const employee = await apiResponse.json();
   
